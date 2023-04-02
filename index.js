5b2c910567edef7f43060096d273d4dc049af259
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-04-02 11:49:52 -0700

    complete assignment

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 main.remove();
 const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = "Mo is the champion";
+newHeader.textContent = "Jim is the champion";
